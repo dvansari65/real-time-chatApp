72f23ddac7ac8ca9e8d96096c3ad675f96d0584d
Danish Ansari <dvansari360@gmail.com>
2025-08-05 20:13:43 +0530

    creatd frontedn socket.ts server

---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: ["error"], 
+  });
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
