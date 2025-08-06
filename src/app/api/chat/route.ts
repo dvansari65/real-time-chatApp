52fae7a1cba14d4df245d95849f7a4ad9bf672c3
Danish Ansari <dvansari360@gmail.com>
2025-08-06 17:36:30 +0530

    created post api for chat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,97 @@
 import { NextResponse } from "next/server";
-
+import {prisma} from "../../../lib/prisma"
 
 export const POST = async(req:NextResponse)=>{
     try {
-        
+        const body = await req.json()
+        const {userID1 , userID2} = body
+        const chat = await prisma.chat.findFirst({
+            where:{
+                AND:[
+                    {
+                        members:{
+                            some:{
+                                userId:userID1
+                            }
+                        }
+                    },
+                    {
+                        members:{
+                            some:{
+                                userId:userID2
+                            }
+                        }
+                    },
+                    {
+                        members:{
+                            every:{
+                                OR:[
+                                    {userId:userID1},
+                                    {userId:userID2}
+                                ]
+                            }
+                        }
+                    }
+                ]
+            },
+            include:{
+                members:{
+                    include:{
+                        user:{
+                            select:{
+                                username:true,
+                                id:true,
+                                avatar:true,
+                                phoneNumber:true,
+                                isOnline:true
+
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        if(chat){
+            return NextResponse.json({chat})
+        }
+        const newChat = await prisma.chat.create({
+            data:{
+                name:"",
+                discription:"",
+                createdAt:new Date,
+                members:{
+                    create:[
+                        {userId:userID1},
+                        {userId:userID2}
+                    ]
+                }
+            },
+            include:{
+                members:{
+                    include:{
+                        user:{
+                            select:{
+                                username:true,
+                                avatar:true,
+                                phoneNumber:true,
+                                isOnline:true,
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        return NextResponse.json(
+            {
+                success:true,
+                chat:newChat
+            }
+        )
     } catch (error) {
-        
+        console.log("failed to create chat!",error )
+        return NextResponse.json(
+            {error:"failed to create chat!"},
+            {status:500}
+        )
     }
 }
