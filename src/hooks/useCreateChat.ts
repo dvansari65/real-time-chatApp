0df2f76c114ef8bbe3cc538b1f34cc6c1c72e632
Danish Ansari <dvansari360@gmail.com>
2025-08-07 19:48:22 +0530

    created hook

---
+++ src/hooks/useCreateChat.ts
@@ -0,0 +1,15 @@
+"use client"
+import { useState } from "react"
+
+
+
+export const useCreateChat = async ()=>{
+    const [laoding,setLoading] = useState(false)
+    const [error,setError] = useState("")
+    const [data , setData] = useState()
+    try {
+        
+    } catch (error) {
+        
+    }
+}
