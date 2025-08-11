7e0294963823da11e38e8a7640166138d033ee9d
Danish Ansari <dvansari360@gmail.com>
2025-08-11 19:59:03 +0530

    init

---
+++ src/lib/api/useGetchat.ts
@@ -0,0 +1,27 @@
+import { chatType } from "@/types/chat";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
+
+export const useGetChat = (chatId:number) => {
+  
+  return useQuery<chatType>({
+    queryKey: ["chat", chatId],
+    queryFn: async () => {
+      const res = await fetch(`/api/chat/${chatId}`, {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = await res.json();
+      console.log("chat dta:",data);
+      
+      if (!res.ok) {
+        toast.error(data.message || "failed to get response!");
+      }
+      return data;
+    },
+    enabled: !!chatId,
+    staleTime: 20 * 60 * 60,
+    retry: 2,
+  });
+};
