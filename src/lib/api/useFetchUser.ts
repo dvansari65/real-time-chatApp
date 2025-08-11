3c469bd25b81946cf07b212a1aa3f898006ee688
Danish Ansari <dvansari360@gmail.com>
2025-08-11 19:58:45 +0530

    init

---
+++ src/lib/api/useFetchUser.ts
@@ -0,0 +1,23 @@
+"use client";
+
+import { useFetchResponse } from "@/types/useFetchUserResponse";
+import { allUsersResponse } from "@/types/user";
+
+import { useQuery } from "@tanstack/react-query";
+
+
+export const useFetchUsers = () => {
+  return useQuery<useFetchResponse>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("/api/users", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (!res.ok) throw new Error("failed to fetch users!");
+      const data = await res.json();
+      return data;
+    },
+    staleTime: 5*60 * 1000
+  });
+};
