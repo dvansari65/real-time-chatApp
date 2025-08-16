d2a428ee74125e9737e5bbc7c0c388dec1ed99c8
Danish Ansari <dvansari360@gmail.com>
2025-08-16 19:03:47 +0530

    created SelectUserForNewGroup.tsx for creating group

--- src/components/modal/SelectUserForNewGroup.tsx
+++ src/components/modal/SelectUserForNewGroup.tsx
@@ -1,12 +1,13 @@
-import { User } from "@/types/user";
+import { partialUser, User } from "@/types/user";
 import { ArrowLeft } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 interface newGroupProps {
   isOpen: boolean;
   onCloce: () => void;
   proceedAction: () => void;
-  users: User[];
+  users: partialUser[];
+  className?: string;
 }
 
 function SelectUserForNewGroup({
@@ -14,36 +15,52 @@ function SelectUserForNewGroup({
   onCloce,
   proceedAction,
   users,
+  className,
 }: newGroupProps) {
+  const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
   if (!isOpen) return null;
+  const handleSelectUsers = (id: number) => {
+    setSelectedUsers((prev) =>
+      selectedUsers.includes(id)
+        ? prev.filter((userId) => userId !== id)
+        : [...prev, id]
+    );
+  };
+
   return (
-    <div className="fixed inset-0 z-50 h-screen max-h-[400px] bg-slate-200 ">
+    <div
+      className={`fixed inset-0  h-[100%] max-w-[330px] bg-slate-100 overflow-y-auto  ${className}`}
+    >
       <div className="flex flex-col gap-3 w-full h-screen">
         <div className="flex flex-row justify-between items-center px-2 py-1 ">
           <button onClick={onCloce}>
             <ArrowLeft size={15} />
           </button>
           <div className="text-black font-bold">Add Members</div>
-          <button onClick={proceedAction} className="text-green-500">
+          <button disabled={selectedUsers.length === 0} onClick={proceedAction} className={ selectedUsers.length === 0 ? `text-gray-500` : `text-green-500`}>
             Next
           </button>
         </div>
         <div>
-            <select name="" id="">
-                {
-                    users.map(user=>(
-                       <div key={user?.id} className="flex flex-row justify-between px-2">
-                        <div className="flex justify-start items-center gap-2 px-1">
-                            <img className="size-16 rounded-[50%] " src={user?.avatar}  />
-                            <span>{user?.username}</span>
-                        </div>
-                        <div>
-                            <input type="checkbox" onChange={} />
-                        </div>
-                       </div>
-                    ))
-                }
-            </select>
+          {users.map((user) => (
+            <div
+              key={user?.id}
+              className="flex flex-row justify-between px-2 border-b border-gray-300 mx-2 py-1 "
+            >
+              <div className="flex justify-start items-center gap-2 px-1">
+                <img className="size-13 rounded-[50%] " src={user?.avatar} />
+                <span>{user?.username}</span>
+              </div>
+              <div className=" flex justify-center items-center">
+                <input
+                  checked={selectedUsers?.includes(Number(user?.id))}
+                  type="checkbox"
+                  onChange={() => handleSelectUsers(Number(user?.id))}
+                  className="w-4 h-4 bg-gray-200 outline-none"
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
