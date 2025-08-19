83e6984dabe46c704d78228584a47dd182da5410
Danish Ansari <dvansari360@gmail.com>
2025-08-19 14:37:57 +0530

    created laoding slice

---
+++ src/features/Redux/loadingSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+interface loadingForCreatingChatProps {
+    isLoading:boolean
+}
+
+const initialState:loadingForCreatingChatProps = {
+    isLoading:false
+}
+
+
+export const loadingSlice = createSlice({
+    name:"Loading",
+    initialState,
+    reducers:{
+        setLoading:(state,action:PayloadAction<boolean>)=>{
+            state.isLoading === action.payload
+        }
+    }
+})
+
+export const {setLoading} = loadingSlice.actions
+export default loadingSlice.reducer
