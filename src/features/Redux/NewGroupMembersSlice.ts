9820fc2f125d97d49bb9319fc952a7b4c878376f
Danish Ansari <dvansari360@gmail.com>
2025-08-21 11:40:59 +0530

    created redux state for new group members

---
+++ src/features/Redux/NewGroupMembersSlice.ts
@@ -0,0 +1,31 @@
+import {  createGroupInputTypesForFrontend } from "@/types/CreateGroup";
+import { partialUser, User } from "@/types/user";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+
+
+const initialState:createGroupInputTypesForFrontend = {
+    userId:null,
+    discription:"",
+    name:"",
+    GroupMembers:[],
+    admins:[],
+    profileImage:""
+} 
+
+export const groupSlice = createSlice({
+    name:"NewGroup",
+    initialState,
+    reducers:{
+        setGroupMembers : (state , action:PayloadAction<partialUser[]>)=>{
+           state.GroupMembers = action.payload
+        },
+        removeUsers : (state,action:PayloadAction<number>)=>{
+            state.GroupMembers?.filter(user => user.id !== action.payload)
+        }
+    }
+})
+
+
+export const {setGroupMembers,removeUsers} = groupSlice.actions
+export default groupSlice.reducer
