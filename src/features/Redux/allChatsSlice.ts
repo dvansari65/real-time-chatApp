928e21fb4d3c114c1a883225b992652433f1cc89
Danish Ansari <dvansari360@gmail.com>
2025-08-29 09:27:43 +0530

    created global state for all chats data

--- src/features/Redux/allChatsSlice.ts
+++ src/features/Redux/allChatsSlice.ts
@@ -1,9 +1,31 @@
-import { messageDataFromGetAllChatsResponse } from "@/types/message"
+import { userFromChat } from "@/types/chat"
+import { Message, messageDataFromGetAllChatsResponse } from "@/types/message"
+import { partialUser } from "@/types/user"
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface initialStateData {
-    messages:messageDataFromGetAllChatsResponse[]
+    messages:Message[] | undefined
+    user:partialUser | null
 }
-const initialState = {
+const initialState:initialStateData = {
     messages:[],
-
+    user:null
 }
+
+export const allChatDataSlice = createSlice({
+    name:"allChatData",
+    initialState,
+    reducers:{
+        storeMessages : (state,action:PayloadAction<Message[] >)=>{
+            state.messages = action.payload
+        },
+        storeUser : (state,action:PayloadAction<partialUser>)=>{
+            state.user = action.payload
+        }
+    }
+})
+
+export const {storeMessages,storeUser} = allChatDataSlice.actions
+export default allChatDataSlice.reducer
+
