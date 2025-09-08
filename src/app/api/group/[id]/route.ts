3bde1d339099aa1616a8b57e50aa2c2aedc41284
Danish Ansari <dvansari360@gmail.com>
2025-09-08 10:34:54 +0530

    created api to get group info

---
+++ src/app/api/group/[id]/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import {prisma} from  "../../../../lib/prisma"
+
+export const GET = async (req:NextRequest,{params}:{params:Promise<{id:string}>})=>{
+    try {
+        const groupId = Number((await params).id)
+        const group = await prisma.group.findFirst({
+            where:{
+                id:groupId
+            },
+            include:{
+                GroupMembers:{
+                    select:{
+                        username:true,
+                        id:true,
+                        avatar:true,
+                    }
+                },
+                admins:{
+                    select:{
+                        username:true,
+                        id:true,
+                        avatar:true,
+                    }
+                }
+            }
+        })
+        if(!group){
+            return NextResponse.json(
+                {
+                    message:"group not found!",
+                    success:false
+                },
+                {status:404}
+            )
+        }
+        return NextResponse.json(
+            {
+                success:true,
+                message:"group fetched successfully!",
+                group
+            },
+            {status:200}
+        )
+    } catch (error) {
+        console.log("failed to fetch group!",error)
+        throw error;
+    }
+}
