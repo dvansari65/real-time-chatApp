e3fb7f57b209a5e7943f4b4524beeab4d1ba665b
Danish Ansari <dvansari360@gmail.com>
2025-09-09 10:15:38 +0530

    update:updated GroupChatItem.tsx component

--- src/components/chat/GroupChat/GroupChatItem.tsx
+++ src/components/chat/GroupChat/GroupChatItem.tsx
@@ -1,34 +1,55 @@
-import { Chat } from '@/types/chat';
+import { Button } from '@/components/ui/Button';
+import { Chat, userFromChat } from '@/types/chat';
+import { Message, messageStatus } from '@/types/message';
 import { Users } from 'lucide-react'
 import React from 'react'
 
+
 interface GroupChatItemProps {
-    groupName:string,
-    updatedAt?:Date;
-    chatResponse:Chat
+  groupName:string,
+  id?: number;
+  name?: string;
+  isGroup?:boolean,
+  createdAt?: Date;
+  updatedAt?: string;
+  description?: string;
+  messages?: Message[];
+  members?: userFromChat[];
+  createChatForGroup:()=>void
 }
 
-function GroupChatItem({groupName,updatedAt,chatResponse}:GroupChatItemProps) {
+function GroupChatItem({groupName,updatedAt,createChatForGroup,messages,members}:GroupChatItemProps) {
+  const formateDate= (dateString?:string)=>{
+    if(!dateString) return new Date();
+    return new Date(dateString).toLocaleDateString()
+  }
+  const latestMessages = messages?.map(message=>({
+    sender:message?.sender,
+    message:message?.content[0]
+  }))
   return (
     <div className="flex items-center gap-4 w-full ">
         <div>
-          <Users size={38} />
+          <Users className='text-gray-500 ml-2' size={34} />
         </div>
-        <div className="flex flex-col  justify-center w-full">
+        <Button onClick={createChatForGroup} className="flex flex-col  justify-center w-full">
           <div className="w-full flex justify-between ">
-            <div className="text-xl text-gray-300">{groupName}</div>
-            <div>{updatedAt?.toLocaleDateString()}</div>
+            <div className="text-sm text-purple-400">{groupName}</div>
+            <div className='text-[10px] text-gray-400'>{formateDate(updatedAt) as string}</div>
           </div>
           <div className="w-full flex justify-between items-center">
-            <div className="text-[15px] text-gray-500">Alice:hey there..</div>
+            <div className="text-[15px] text-gray-500 flex items-center ">
+              <span>{latestMessages?.[0]?.sender || "Last message preview..."}</span>
+              <span>{latestMessages?.[0]?.message}</span>
+            </div>
             <div className="flex items-center gap-1">
               <Users className="text-gray-500" size={12} />
               <p className="text-gray-500 text-[14px]">
-                {chatResponse?.members?.length}
+                {members?.length}
               </p>
             </div>
           </div>
-        </div>
+        </Button>
       </div>
   )
 }
