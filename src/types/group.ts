c71f8b9645162276e5c03c47eb86c68a0dfaedda
Danish Ansari <dvansari360@gmail.com>
2025-09-09 12:40:34 +0530

    update:Added types for group

---
+++ src/types/group.ts
@@ -0,0 +1,20 @@
+import { Chat } from "./chat";
+import { partialUser } from "./user"
+
+export interface Group {
+    id?:number,         
+    profileImage?:string, 
+    userId?:number,       
+    GroupMembers?:partialUser[] 
+    name?:string;         
+    discription?:string,  
+    admins?:partialUser[],       
+    createdBy?:string    
+    chat ?:Chat       
+  }
+
+export interface getSingleGroupResponse {
+    success:boolean,
+    message:string,
+    group:Group
+}
