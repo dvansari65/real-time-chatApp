5865958b9b484dffb6172740b258a25c1224efc4
Danish Ansari <dvansari360@gmail.com>
2025-11-10 15:16:13 +0530

    created bottom gradient component

---
+++ src/components/ui/Sidebars/BottomGradient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BottomGradient() {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-gray-900/80 to-transparent pointer-events-none"></div>
+  )
+}
+
+export default BottomGradient
