28282d52420ca732aa5b20f98cb92669d9a2e615
Danish Ansari <dvansari360@gmail.com>
2025-11-10 15:11:32 +0530

    created animated componet for the bg

---
+++ src/components/ui/AnimatedBG.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function AnimatedBG() {
+  return (
+    <div className="absolute inset-0 overflow-hidden">
+        {[...Array(8)].map((_, i) => (
+          <div
+            key={i}
+            className="absolute w-1 h-1 bg-blue-500/20 rounded-full animate-pulse"
+            style={{
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 3}s`,
+              animationDuration: `${2 + Math.random() * 3}s`,
+            }}
+          ></div>
+        ))}
+      </div>
+  )
+}
+
+export default AnimatedBG
